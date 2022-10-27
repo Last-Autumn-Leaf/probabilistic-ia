ff5d843f982c4dfd70b683aaaa4de9ca7d5045b8
TheShyDev-Yoogo <yoogo.production@gmail.com>
2022-10-27 18:53:21 -0400

    Bayes 85 % Let's goo

--- problematique/test2.py
+++ problematique/test2.py
@@ -1,17 +1,28 @@
 import numpy as np
-np.random.seed(0)
 from helpers import classifiers
 import matplotlib.pyplot  as plt
 from helpers.custom_class import ClassesTracker
 
-if __name__=='__main__':
 
+def Bayes():
     CT = ClassesTracker()
-    train_data,train_classes, test_data, test_classes=CT.get_data_classwise(n=0.8)
+    train_data, train_classes, test_data, test_classes = CT.get_data_classwise(n=0.8)
     classifiers.full_Bayes_risk(train_data, train_classes, CT.donneesTest, 'Bayes risque #1',
                                 CT.extent, test_data, test_classes)
-
     plt.show()
+
+
+def KNN():
+    CT = ClassesTracker()
+    train_data, train_classes, test_data, test_classes = CT.get_data_classwise(n=0.8)
+    classifiers.full_Bayes_risk(train_data, train_classes, CT.donneesTest, 'Bayes risque #1',
+                                CT.extent, test_data, test_classes)
+    plt.show()
+
+
+if __name__=='__main__':
+    np.random.seed(0)
+    Bayes()
 
 
 
